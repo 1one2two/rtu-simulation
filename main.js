e9758adb8ecaf64904c7c3e8479b8c55eaa1d9a4
1one2two <ej04vm6@gmail.com>
2019-07-24 18:04:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ var fetch = require('cross-fetch')
 var key = 'fl23n2xx7uz5uj6kvrzw'
 const scanner = require('node-wifi-scanner');
 
-execute('npm install wifiscanner')
+//execute('npm install wifiscanner')
 
-const client = require('./lib/client')
+//const client = require('./lib/client')
 
-var m = JSON.parse(`{"rtu":{"model":"anko-tpe","serial":"odn12345678","loc":"23,117"},"xyz":{"z01":{"name":"temp","unit":"℃","range":[20,90],"warning":[70,90]},"z02":{"name":"current","unit":"mA","range":[0,30],"warning":[20,30]}},"dreg":{"d01":{"name":"temp","unit":"℃","range":[20,90],"warning":[70,90]},"d02":{"name":"current","unit":"mA","range":[0,30],"warning":[20,30]}},"timestamp":{"repeat":"1","interval":"10"},"data":{},"notify":{"ddn":">>comm","topic":"tg://-352168287","payload":""},"log":{"ddn":">>#anko-view","topic":"","payload":""}}`);
+var m = JSON.parse(`{"rtu":{"model":"anko-tpe","serial":"odn12345678","loc":"23,117"},"xyz":{"z01":{"name":"temp","unit":"℃","range":[20,90],"warning":[70,90]},"z02":{"name":"current","unit":"mA","range":[0,30],"warning":[20,30]}},"dreg":{"d01":{"name":"temp","unit":"℃","range":[20,90],"warning":[70,90]},"d02":{"name":"current","unit":"mA","range":[0,30],"warning":[20,30]}},"timestamp":{"repeat":"1","interval":"10"},"data":{},"notify":{"ddn":">>comm","topic":"tg://-352168287","payload":""},"log":{"ddn":">>#anko-view","topic":"","payload":""},"reportmode":2}`);
 
 function execute(command) {
     const exec = require('child_process').exec
@@ -27,7 +27,7 @@ function getWifiInfo() {
             var re = `{"wifiAccessPoints": [`
             for (var i in networks) {
                 if (i < 15)
-                re = re + (`{"macAddress":"${networks[i].mac}", "signalStrength":${networks[i].rssi}},`)
+                    re = re + (`{"macAddress":"${networks[i].mac}", "signalStrength":${networks[i].rssi}},`)
             }
             resolve(re)
         });
@@ -48,17 +48,20 @@ async function get_loc() {
 
 function rd(lower, upper) { return Math.floor(Math.random() * (upper - lower)) + lower }
 
+//0: all 1: error only 2:error+warning 3:none
 function ru() {
-    var s = []
+    var s = [];
+    var war = false;
     for (var x in m.xyz) {
         let val = rd(m.xyz[x].range["0"], m.xyz[x].range["1"])
         let warning_lower = m.xyz[x].warning["0"]
         let warning_upper = m.xyz[x].warning["1"]
 
         let warning = val > warning_lower && val < warning_upper ? "*warning*" : ""
+        war = val > warning_lower && val < warning_upper ? true : war
         s.push(`[${x}] ${m.xyz[x].name}: \t${val} ${m.xyz[x].unit}\t${warning}`)
     }
-    return s
+    return [s, war]
 }
 
 async function setup() {
@@ -66,16 +69,33 @@ async function setup() {
     date = (date.getFullYear() + ('00' + (date.getMonth() + 1)).slice(-2) + ('00' + date.getDate()).slice(-2) +
         ('00' + date.getHours()).slice(-2) + ('00' + date.getMinutes()).slice(-2) + "-" +
         ('00' + date.getSeconds()).slice(-2) + ('0' + date.getMilliseconds()).slice(-1))
-	let get_locs = await get_loc();
+    //let get_locs = await get_loc();
     var re_tg = `ID:${date}
 model:  ${m.rtu.model}
 serial:    ${m.rtu.serial}
-loc:          ${JSON.parse(get_locs)}\n`
+loc:          ${m.rtu.loc}\n`
 
-    ru().map((item) => re_tg += item + '\n');
-    console.log(re_tg)
+    var re = ru();
+    re[0].map((item) => re_tg += item + '\n');
+
+    switch (m.reportmode) {
+        case 0:
+            console.log(re_tg)
+            break;
+        case 1:
+            break;
+        case 2:
+            if (re[1]) console.log(re_tg)
+            break;
+        case 3:
+            break;
+    }
+
     let payload_tg = { "type": "message", "content": re_tg }
+    
+    if (m.timestamp.repeat == "1") setTimeout(setup, (m.timestamp.interval * 1000))
 
+    return;
     client.send(m.notify.topic, m.notify.ddn, payload_tg)
 
     var date = new Date()
@@ -88,10 +108,11 @@ loc:          ${JSON.parse(get_locs)}\n`
 "to": "anko-dhq"
 "note": "9"
 "status": 0`
+
     client.send(m.log.topic, m.log.ddn, payload_view)
 
-    if (m.timestamp.repeat == "1") setTimeout(setup, (m.timestamp.interval * 1000))
+    
 }
 
-client.init()
+//client.init()
 setup()
